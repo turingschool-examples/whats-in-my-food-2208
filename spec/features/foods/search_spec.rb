87ca2f2f79c30e06417d33a2267ffaee471f5164
Emily Port <emilylport@gmail.com>
2022-12-16 10:44:05 -0700

    add another search in test

--- spec/features/foods/search_spec.rb
+++ spec/features/foods/search_spec.rb
@@ -10,12 +10,24 @@ RSpec.describe 'food search' do
       expect(page).to have_content("Search Result Total: 49083")
 
       expect(page).to have_content("Description: SWEET POTATO")
+      expect(page).to_not have_content("Description: CHEESE")
+
       expect(page).to have_content("50 results on this page")
       expect(page).to have_content('Results for "sweet potato" search')
       expect(page).to have_content("FDC ID: 477475")
       expect(page).to have_content("Brand Owner: FRESH & EASY")
       expect(page).to have_content("Ingredients: SWEET POTATO, CHIPOTLE CHICKEN")
       expect(page).to have_content("Displaying 10 results")
+
+      visit root_path
+      fill_in (:q), with: "cheese"
+      click_button "Search"
+      expect(current_path).to eq('/foods')
+      expect(page).to have_content("Search Result Total: 54391")
+      expect(page).to have_content("Description: CHEESE")
+      expect(page).to_not have_content("Description: SWEET POTATO")
+
+
 
     end
     
