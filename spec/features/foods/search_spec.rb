e0fc08325327af310abd5bbdc55e6606be921958
Astrid Hecht <silentaviary@gmail.com>
2022-12-16 09:51:32 -0700

    test: food search feature test complete

--- spec/features/foods/search_spec.rb
+++ spec/features/foods/search_spec.rb
@@ -16,32 +16,47 @@ RSpec.describe 'Food search page', :vcr, type: :feature do
         end
 
         it 'Then I should see a total of the number of items returned by the search.' do
-          expect(page).to have_x
-          expect(page).to have_x
-          expect(page).to have_x
+          expect(page).to have_selector('#result-count')
+          within '#result-count' do
+            expect(page).to match(/^(\d+|\d{1,3}(,\d{3})*)(\.\d+)?$/)
+            expect(page.gsub(',', '_').to_i).to >= 30_000
+          end
         end
 
         it 'Then I should see a list of TEN foods that contain the ingredient "sweet potatoes"' do
-          expect(page).to have_x
-          expect(page).to have_x
-          expect(page).to have_x
+          expect(page).to have_selector('#results-table')
+          within '#results-table' do
+            expect(page).to have_selector('.food')
+            page.assert_selector(:css, '.food', count: 10)
+          end
         end
 
         context 'And for each of the foods I should see:' do
+          let!(:first) do
+            within '#results-table' do
+              page.find('.food', match: :first)
+            end
+          end
+
           it "The food's GTIN/UPC code" do
-            expect(page).to have_x
+            expect(first).to have_selector('.code')
+            expect(first.find('.code').text).not_to match(/\D/)
           end
 
           it "The food's description" do
-            expect(page).to have_x
+            expect(first).to have_selector('.description')
+            expect(first.find('.description').text).to be_a(String)
           end
 
           it "The food's Brand Owner" do
-            expect(page).to have_x
+            expect(first).to have_selector('.owner')
+            expect(first.find('.owner').text).to be_a(String)
           end
 
           it "The food's ingredients" do
-            expect(page).to have_x
+            expect(first).to have_selector('.ingredients')
+            expect(first.find('.ingredients').text).to be_a(String)
+            expect(first.find('.ingredients').text.split(', ')).to be_a(Array)
           end
         end
       end
