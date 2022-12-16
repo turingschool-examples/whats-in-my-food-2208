acf2020ff2dd3777bc0e9e96aaa92675da69fe90
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-12-16 10:56:11 -0700

    Add poros file and test

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,7 @@
 class FoodsController < ApplicationController
   def search
     foods = FoodService.new.find_food(params[:q])
+    require 'pry'; binding.pry
     @all_foods = foods[:foods]
     # require 'pry'; binding.pry
     @total_items = foods[:totalHits]
