8e23ec1403f02504f2cce96d13e1612241a21fc9
Mackenzie Leng <kenz.joy@outlook.com>
2022-12-16 09:31:53 -0700

    adds api key and creates a foods index spec shell test

---
+++ spec/features/foods/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'food index page' do
+  describe 'as a user' do
+    describe 'when I visit foods_path' do
+      it 'shows the total number of items returned by the search' do
+
+      end
+      
+      it 'only shows a list of ten foods that contain the ingredient' do
+
+      end
+
+      it 'each food shows its GTIN/UPC code, its description, its brand owner, and its ingredients' do
+        
+      end
+    end
+  end
+end
