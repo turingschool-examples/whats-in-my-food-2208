7c05d7ce2dea33ea6b9677ccc6fea43e825d24c5
Darby Smith <newman.darby2@gmail.com>
2022-12-16 12:37:21 -0500

    Move count

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,6 @@
 class FoodsController < ApplicationController
   def search
-    @foods = FoodFacade.food_finder(params[:q])
+    @foods = FoodFacade.food_finder(params[:q]).take(10)
     @count = FoodFacade.count_responses(params[:q])
   end
 end
