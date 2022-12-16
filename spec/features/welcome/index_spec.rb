156ca35fbfb6324a008e753f7f23807367e33245
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-16 10:19:18 -0700

    Add foods controller

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Welcome Page", type: :feature do
       fill_in 'q', with: 'sweet potatoes'
       click_button 'Search'
 
-      expect(page).to have_current_path('/foods')
+      expect(page).to have_current_path('/foods?utf8=%E2%9C%93&q=sweet+potatoes&commit=Search')
     end
   end
 end
