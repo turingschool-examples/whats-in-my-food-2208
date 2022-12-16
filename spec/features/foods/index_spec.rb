38d3139c8b3e7869ff1352d8926dc1a8f6f30969
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-16 09:41:48 -0700

    Write food index spec test 2 of 4

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -15,7 +15,9 @@ describe 'The Foods Index Page' do
         it 'I am on the "/foods" page' do
           expect(current_path).to eq(foods_path)
         end
-        it 'I see a total number of items returned by the search'
+        it 'I see a total number of items returned by the search' do
+          expect(page).to have_content('Total number of items: 49083')
+        end
         it 'I see a list of 10 foods that contain the ingredient "sweet potatoes"'
         it "I see each food's GTIN/UPC code, description, Brand Owner, and ingredients"
       end
