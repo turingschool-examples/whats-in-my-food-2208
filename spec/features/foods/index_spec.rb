99fd25479e1710b264f30102ed5097fc41865d4b
Gabe Nunez <gabriel.c.nunez@gmail.com>
2022-12-16 09:25:56 -0700

    Add before each block to foods index spec, write test 1 of 4

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -4,7 +4,17 @@ describe 'The Foods Index Page' do
   context 'As a user' do
     describe 'When I visit the Welcome page' do
       describe 'I fill in the search form with "sweet potatoes" and click "Search"' do
-        it 'I am on the "/foods" page'
+        before :each do
+          visit '/'
+
+          fill_in :q, with: 'sweet potatoes'
+
+          click_button 'Search'
+        end
+
+        it 'I am on the "/foods" page' do
+          expect(current_path).to eq(foods_path)
+        end
         it 'I see a total number of items returned by the search'
         it 'I see a list of 10 foods that contain the ingredient "sweet potatoes"'
         it "I see each food's GTIN/UPC code, description, Brand Owner, and ingredients"
