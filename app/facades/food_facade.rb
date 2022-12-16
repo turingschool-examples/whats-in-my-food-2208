31773f81f78ff7076fa30fcde76991ca1c0ce871
Shawn Lee <Shawnl93@hotmail.com>
2022-12-16 09:41:17 -0700

    complete setting up poros

--- app/facades/food_facade.rb
+++ app/facades/food_facade.rb
@@ -1,7 +1,7 @@
 class FoodFacade
     def self.search_food(search)
         data = FoodService.search_food(search)
-        data[:pageList].map do |food_data|
+        data[:foods].map do |food_data|
             FoodInfo.new(food_data)
         end
     end
