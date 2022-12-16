cc979dc18174ac369ca35cc37a3502f39c8ac463
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-16 10:37:52 -0700

    Add food search service

--- app/services/fdc_services.rb
+++ app/services/fdc_services.rb
@@ -1,14 +1,17 @@
 class FdcService
+  def food_search(query)
+    get_url('/foods/search', {query: query})
+  end
+
+  def get_url(url, params = nil)
+    response = connection.get(url, params)
+    JSON.parse(response.body, symbolize_names: true) 
+  end
+
   def connection
     Faraday.new(url: "https://api.nal.usda.gov/fdc/v1") do |faraday|
       faraday.params['api_key'] = ENV['fdc_key']
       faraday.params['language'] = 'en-US'
     end
   end
-  
-  def get_url(url, params = nil)
-    response = connection.get(url, params)
-    JSON.parse(response.body, symbolize_names: true) 
-  end
-  
 end
---
+++ spec/poros/food_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Food" do
+  describe 'attributes' do
+    xit 'can be created from a hash' do
+
+    end
+    xit 'has ' do
+
+    end
+  end
+end
