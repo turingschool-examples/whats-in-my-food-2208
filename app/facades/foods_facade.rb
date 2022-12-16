0fa9905d137a6feed89b8bd6f9a935c90812307e
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-16 11:01:47 -0700

    Final commit

--- app/facades/foods_facade.rb
+++ app/facades/foods_facade.rb
@@ -3,6 +3,11 @@ require './app/services/fdc_services'
 class FoodsFacade
   def food_results(query)
     response = FdcService.new.food_search(query)
-    foods_hash = response[:foods]
+    foods_hash = response[:foods].first(10)
+    foods_hash.map { |hash| Food.new(hash) }
+  end
+  def results_count(query)
+    response = FdcService.new.food_search(query)
+    response[:totalHits]
   end
 end
