83b8940d94e9de6634c224cc5ed8331e37646da1
Ashley <ashuhleyt@gmail.com>
2022-12-16 08:34:22 -0800

    setting up api call

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'welcome index' do 
-  xit 'i fill in the form with sweet potatoes and click search' do 
+  it 'i fill in the form with sweet potatoes and click search' do 
     visit root_path
-    fill_in("Ingredient Search", with: "Sweet Potato")
+    fill_in(:q, with: "Sweet Potato") #this took me FAR too long. :(
     click_button("Search")
 
     expect(current_path).to eq(foods_path)
@@ -12,6 +12,6 @@ RSpec.describe 'welcome index' do
   it 'i see a total number of items returned from my search' do 
     visit foods_path
 
-    expect(current_page).to have_content
+    expect(current_page).to have_content(tmyUurfe2wJOKLnvfz4ldgjp9dpKcIGCDaaHXGaR)
   end
 end
