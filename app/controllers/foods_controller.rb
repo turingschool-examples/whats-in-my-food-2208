5e8cb1ab152b001281cca203ac9002d791d94401
Naomi Yocum <naomiyocum24@gmail.com>
2022-12-16 10:22:53 -0700

    Add formatting to include user search

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,13 @@
 class FoodsController < ApplicationController
+  before_action :user_search
+  
   def index
     @search = FoodFacade.get_foods_with(params[:q])
+  end
+
+  private
+
+  def user_search
+    @user_search = params[:q]
   end
 end
