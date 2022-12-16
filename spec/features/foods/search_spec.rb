784ced13abd87a6954ce99cbc4f63fb64e08efd4
Madeline Mauser <madeline.mauser@gmail.com>
2022-12-16 10:05:21 -0700

    Add test for foods page

--- spec/features/foods/search_spec.rb
+++ spec/features/foods/search_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
 
-RSpec.feature "Welcome Page", type: :feature do
-  describe 'search form' do
-    it 'has a form that redirects to /foods when a query is submitted' do
-      visit '/foods'
-      save_and_open_page
+RSpec.feature "Foods Page", type: :feature do
+  describe 'contents' do
+    before :each do
+      visit '/'
+      fill_in 'q', with: 'sweet potatoes'
+      click_button 'Search'
+    end
+    it 'has a count of total hits for the search' do
+      expect(page).to have_content("Number of results: 49083")
+    end
+    it 'should have the GTN/UPC code of each food' do
+      expect(page).to have_content("GTIN/UPC code: 832298010009")
+    end
+    it 'should have the description of each food' do
+      expect(page).to have_content("Description: SWEET POTATOES")
+    end
+    it 'should have the brand owner of each food' do
+      expect(page).to have_content("Brand Owner: NOT A BRANDED ITEM")
+    end
+    it 'should list ten of the food results' do
+      expect(page).to have_content("GTIN/UPC code:", count: 10)
     end
   end
 end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -5,7 +5,8 @@ RSpec.feature "Welcome Page", type: :feature do
     it 'has a form that redirects to /foods when a query is submitted' do
       visit '/'
       fill_in 'q', with: 'sweet potatoes'
-      
+      click_button 'Search'
+
       expect(page).to have_current_path('/foods')
     end
   end
