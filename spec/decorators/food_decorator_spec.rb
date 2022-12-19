161a00926cd6962615b63d16b9f0a52d26323062
Joseph Hilby <joseph.hilby@gmail.com>
2022-12-19 15:03:08 -0600

    Update FoodDecorator spec file

--- spec/decorators/food_decorator_spec.rb
+++ spec/decorators/food_decorator_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe FoodDecorator do
   describe 'instance methods' do
     describe '#foods?' do
-      it 'returns a bool value of true if there were any food returns', :vcr do
+      it 'returns a bool value of true or false if there were any food returns or not', :vcr do
         expect(FoodDecorator.new(FoodFacade.new('sweet potatoes')).foods?).to eq(true)
         expect(FoodDecorator.new(FoodFacade.new('xkcd')).foods?).to eq(false)
       end
